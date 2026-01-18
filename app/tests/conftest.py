2ec5ee8a3d6f88df5059b357710b91c98d5ccc7d
Alfredo Aguilar <jose.aguilarlara1999@gmail.com>
2026-01-18 17:43:08 -0600

    Fix: tests async issues and cleanup

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,11 +1,7 @@
 import pytest
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
 
 from app.main import app
 from app.core.database import Base
@@ -13,47 +9,47 @@ from app.dependecies import get_db
 from app.core.config import settings
 from app.core.redis_client import get_redis_client
 import redis.asyncio as redis
-
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+import asyncio
 
 
 @pytest.fixture(scope="session")
 def event_loop():
-    import asyncio
-
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
     yield loop
     loop.close()
 
 
 TEST_DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-engine = create_async_engine(
-    TEST_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-TestingSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest_asyncio.fixture(scope="session")
+async def db_engine():
+    engine = create_async_engine(
+        TEST_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+    yield engine
+
+    await engine.dispose()
 
 
 @pytest_asyncio.fixture(scope="function")
-async def db_session():
+async def db_session(db_engine):
 
-    async with engine.begin() as conn:
+    async with db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+    TestingSessionLocal = async_sessionmaker(
+        autocommit=False, autoflush=False, bind=db_engine
+    )
 
     async with TestingSessionLocal() as session:
         yield session
 
-    async with engine.begin() as conn:
+    async with db_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
